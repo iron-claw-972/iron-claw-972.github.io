45e5ede751db5b04606f2f66ee945325372050a9
Val <valeriy.rotan@gmail.com>
2018-04-03 18:53:43 -0700

    'fixed' the scouting app

--- scout/submit/submit.js
+++ scout/submit/submit.js
@@ -1,9 +1,11 @@
 $('document').ready(function() {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbzJb8ch1pg5vSCx8dAoTHEQwwn5SjfcGsWa7GpP4mb4-C20tq0G/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzJb8ch1pg5vSCx8dAoTHEQwwn5SjfcGsWa7GpP4mb4-C20tq0G/exec?key=1-IviKDX4I6YIn91c9bCKNuz8CDZBbLq02fEz-5fjQcw'
     const form = document.forms['submit-to-google-sheet']
 
     form.addEventListener('submit', e => {
         e.preventDefault()
+        $('#submit').attr('disabled', 'disabled');
+
         fetch(scriptURL, {
                 method: 'POST',
                 body: new FormData(form)
